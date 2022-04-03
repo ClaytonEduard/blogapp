b102edf7ce9a2a97a360a66569c3af1b7d1d4562
claytoneduard~ <clayton_eduard@hotmail.com>
2022-04-02 21:22:48 -0300

    Fomulário Cadastro de Usuário finalizado

--- models/Usuario.js
+++ models/Usuario.js
@@ -10,10 +10,15 @@ const Usuario = new Schema({
     type: String,
     required: true,
   },
+  eAdmin: {
+    type: Number,
+    default: 0,
+  },
   senha: {
     type: String,
     requiredPaths: true,
   },
 });
+// hasheando a senha para ser salva
 
 mongoose.model("usuarios", Usuario);
--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 require("../models/Usuario");
 const Usuario = mongoose.model("usuarios");
+const bcrypt = require("bcryptjs");
 
 router.get("/registro", (req, res) => {
   res.render("usuarios/registro");
@@ -31,17 +32,60 @@ router.post("/registro", (req, res) => {
   ) {
     erros.push({ texto: "Senha inválida" });
   }
-  if (!req.body.senha.length < 4) {
+  if (req.body.senha.length < 4) {
     erros.push({ texto: "Senha muito curta" });
   }
 
-  if (!req.body.senha != req.body.senha2) {
+  if (req.body.senha != req.body.senha2) {
     erros.push({ texto: "As senhas são diferentes, tente novamente!" });
   }
 
   if (erros.length > 0) {
     res.render("usuarios/registro", { erros: erros });
   } else {
+    // verificando se o usuario ja possui cadastro
+    Usuario.findOne({ email: req.body.email })
+      .then((usuario) => {
+        if (usuario) {
+          req.flash("error_msg", "Já existe uma conta com essse e-mail: ");
+          res.redirect("/usuarios/registro");
+        } else {
+          const novoUsuario = new Usuario({
+            nome: req.body.nome,
+            email: req.body.email,
+            senha: req.body.senha,
+          });
+          bcrypt.genSalt(10, (erro, salt) => {
+            bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+              if (erro) {
+                req.flash(
+                  "error_msg",
+                  "Houve um erro durante o salvamento do usuário"
+                );
+                res.redirect("/");
+              }
+              novoUsuario.senha = hash;
+              novoUsuario
+                .save()
+                .then(() => {
+                  req.flash("success_msg", "Usuário criado com sucesso");
+                  res.redirect("/");
+                })
+                .catch((err) => {
+                  req.flash(
+                    "error_msg",
+                    "Houve um erro ao criar um usuário, tente novamente" + err
+                  );
+                  res.redirect("/usuarios/registro");
+                });
+            });
+          });
+        }
+      })
+      .catch((err) => {
+        req.flash("error_msg", "Houve um erro interno: " + err);
+        res.redirect("/");
+      });
   }
 });
 
